19a7a9ff50ffcd5c7401e901a6c8815810c07867
alm <ljl24tyf@bangor.ac.uk>
2024-05-29 23:49:00 +0500

    done

---
+++ app.js
@@ -0,0 +1,6 @@
+
+// This file can be expanded to include more complex AR functionalities
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("AR application initialized.");
+    // Additional JavaScript code for handling AR data and interactions can be added here
+});
